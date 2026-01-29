39cb60f98e2bef9a16bc9d370b3d429e72c19891
SynScoots <admin@winterwind.co.uk>
2026-01-29 21:37:48 +0000

    Now prints to all chat frames with system messages enabled. Defaults to current behaviour if none.

--- src/ScootsRares/ScootsRares.lua
+++ src/ScootsRares/ScootsRares.lua
@@ -1,21 +1,37 @@
-local SR = {}
-SR.frame = CreateFrame('Frame', 'ScootsRaresFrame', UIParent)
+local ScootsRares = {
+    ['version'] = '2.0.0',
+    ['frame'] = CreateFrame('Frame', 'ScootsRaresFrame', UIParent)
+}
 
-function SR.parseChat(msg)
+function ScootsRares.parseChat(msg)
     if(string.find(msg, ' (Rare)', 1, true) and string.find(msg, 'is nearby.', 1, true)) then
         local npc = string.gsub(msg, '|cff99ffff', '')
         npc = string.gsub(npc, ' %(Rare%) |cffccccccis nearby.', '')
-        DEFAULT_CHAT_FRAME:AddMessage('\124HScootsRares:' .. npc .. '\124h[Click here to track ' .. npc .. ']\124h\124r')
+        
+        local message = '\124HScootsRares:' .. npc .. '\124h[Click here to track ' .. npc .. ']\124h\124r'
+        local printed = false
+        
+        for i = 1, NUM_CHAT_WINDOWS do
+            local chatFrame = _G['ChatFrame' .. tostring(i)]
+            if(chatFrame and chatFrame:IsEventRegistered('CHAT_MSG_SYSTEM')) then
+                chatFrame:AddMessage(message)
+                printed = true
+            end
+        end
+        
+        if(printed == false) then
+            DEFAULT_CHAT_FRAME:AddMessage(message)
+        end
     end
 end
 
-function SR.eventHandler(self, event, arg1)
+function ScootsRares.eventHandler(self, event, arg1)
     if(event == 'CHAT_MSG_SYSTEM') then
-        SR.parseChat(arg1)
+        ScootsRares.parseChat(arg1)
     end
 end
 
-function SR.handleLink(link)
+function ScootsRares.handleLink(link)
     local _, npc = strsplit(':', link)
     SendChatMessage('.findnpc ' .. npc, 'SAY')
 end
@@ -25,7 +41,7 @@ do
     
 	function ItemRefTooltip:SetHyperlink(link, ...)
 		if(link:match('^ScootsRares')) then
-            SR.handleLink(link)
+            ScootsRares.handleLink(link)
             return
         end
         
@@ -33,7 +49,6 @@ do
 	end
 end
 
+ScootsRares.frame:SetScript('OnEvent', ScootsRares.eventHandler)
 
-SR.frame:SetScript('OnEvent', SR.eventHandler)
-
-SR.frame:RegisterEvent('CHAT_MSG_SYSTEM')
+ScootsRares.frame:RegisterEvent('CHAT_MSG_SYSTEM')
